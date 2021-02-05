ba6f28b336471eefcf76237df4b24e0d9bfb5b96
WAKUZO <58378273+WAKUZO@users.noreply.github.com>
2021-02-05 10:29:53 +0900

    タスク追加機能テキスト

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // TODOを入力欄のテキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグの生成
+  const div = document.createElement("div");
+  // divタグの中にlist-rowクラスを差し込む
+  div.className = "list-row";
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に、各要素を設定
+  div.appendChild(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
